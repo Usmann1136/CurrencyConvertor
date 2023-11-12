1e1962aaebc01d363f21d2748cdbc8c96299d48c
Usmann1136 <121626247+Usmann1136@users.noreply.github.com>
2023-11-11 22:11:27 -0800

    Add files via upload

---
+++ src/constants.ts
@@ -0,0 +1,133 @@
+export const currencyByRupee: Currency[] = [
+    {
+      name: 'DOLLAR',
+      value: 0.0033,
+      flag: '🇺🇸',
+      symbol: '$',
+    },
+    {
+      name: 'EURO',
+      value: 0.0031,
+      flag: '🇪🇺',
+      symbol: '€',
+    },
+    {
+      name: 'POUND',
+      value: 0.0026,
+      flag: '🇬🇧',
+      symbol: '£',
+    },
+    {
+      name: 'RUBEL',
+      value: 0.32,
+      flag: '🇷🇺',
+      symbol: '₽',
+    },
+    {
+      name: 'AUS DOLLAR',
+      value: 0.0051,
+      flag: '🇦🇺',
+      symbol: 'A$',
+    },
+    {
+      name: 'CAN DOLLAR',
+      value: 0.0045,
+      flag: '🇨🇦',
+      symbol: 'C$',
+    },
+    {
+      name: 'YEN',
+      value: 0.48,
+      flag: '🇯🇵',
+      symbol: '¥',
+    },
+    {
+      name: 'DINAR',
+      value: 0.0010,
+      flag: '🇰🇼',
+      symbol: 'KD',
+    },
+    {
+      name: 'BITCOIN',
+      value: 0.000000543544886,
+      flag: '🎰',
+      symbol: '₿',
+    },
+    {
+        name: 'YUAN',
+        value: 0.024 ,
+        flag: '🇨🇳',
+        symbol: '¥',
+      },
+      {
+        name: 'RUPIAH',
+        value: 50.26,
+        flag: '🇮🇩',
+        symbol: 'Rp',
+      },
+      {
+        name: 'SWISS FRANC',
+        value: 0.0029 ,
+        flag: '🇨🇭',
+        symbol: 'CHF',
+      },
+    
+      {
+        name: 'REAL',
+        value: 0.016,
+        flag: '🇧🇷',
+        symbol: 'R$',
+      },
+      {
+        name: 'RAND',
+        value: 0.062 ,
+        flag: '🇿🇦',
+        symbol: 'R',
+      },
+      // Add more currencies here
+      {
+        name: 'BAHT',
+        value: 0.12 ,
+        flag: '🇹🇭',
+        symbol: '฿',
+      },
+      {
+        name: 'KRONA',
+        value: 0.036,
+        flag: '🇸🇪',
+        symbol: 'kr',
+      },
+      {
+        name: 'DONG',
+        value: 78.60,
+        flag: '🇻🇳',
+        symbol: '₫',
+      },
+      {
+        name: 'INR RUPEE',
+        value: 0.27,
+        flag: '🇮🇳',
+        symbol: '₹',
+      },
+     
+      {
+        name: 'TURK LIRA',
+        value: 0.088 , 
+        flag: '🇹🇷', 
+        symbol: '₺', 
+      },
+      {
+        name: 'EGYPT POUND',
+        value: 0.10, // Replace with the actual exchange rate
+        flag: '🇪🇬', // Egyptian flag
+        symbol: '£', // Egyptian Pound symbol
+      },
+      {
+        name: 'PHILIPPINE PESO',
+        value: 0.19, // Replace with the actual exchange rate
+        flag: '🇵🇭', // Philippine flag
+        symbol: '₱', // Philippine Peso symbol
+      },
+    
+  ];
+  
